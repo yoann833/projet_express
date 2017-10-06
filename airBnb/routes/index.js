2297db935ab52dac4086f58332700934bffe02c8
BrunoBTS2B <bruno.cambianica@gmail.com>
2017-10-06 16:50:27 +0200

    Je vais le faire
    J'ai plus rien à perdre

--- airBnb/routes/index.js
+++ airBnb/routes/index.js
@@ -1,15 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+
+var data = 
+{
+    Boston: {
+                  Hotel1 : 'Le Premier',
+                  Hotel2 : 'Palas Royal'
+              },
+    NewYork: {
+                Hotel1: 'Hotel un' ,
+                Hotel2: 'Hotel deux'
+              }
+};
+
+//console.log(data);
+//console.log(JSON.stringify(data.Boston.Hotel1))
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+// Login page
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Connexion -->' });
 });
+
+//destination page
+router.get('/destination', function(req, res, next) {
+  res.render('destination', data);
+});
+
 
 router.post('/post_form', function(req, res, next) {
 
@@ -27,5 +48,15 @@ router.post('/post_form', function(req, res, next) {
   
   
 });
+
+// redirection destination 
+router.post('/search/:value', function(req, res, next) {
+
+  console.log(req.params.value);
+  console.log(req.body.value);
+  res.redirect('/destination');
+});
+
+
 
 module.exports = router;
