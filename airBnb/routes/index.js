6ac9a496bc35e735791f732889e35fbbdba2f3ee
yoann833 <32564664+yoann833@users.noreply.github.com>
2017-10-06 15:46:25 +0200

    Add files via upload

---
+++ airBnb/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Connexion -->' });
+});
+
+router.post('/post_form', function(req, res, next) {
+
+  if(req.body.login == "coucou" && req.body.password == "azerty")
+  {
+    console.log('authentified')
+    res.redirect('/');
+  }
+  else
+  {
+    console.log('not authentified')
+    res.redirect('/login');
+  }
+  console.log(JSON.stringify(req.body.login));
+  
+  
+});
+
+module.exports = router;
